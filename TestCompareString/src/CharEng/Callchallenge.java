2a70cead73292a4fd586cb51c5a08a390d7ee80b
Haleluyaa <Haleluyaa.aki@gmail.com>
2019-12-10 10:43:05 +0700

    First commit

---
+++ TestCompareString/src/CharEng/Callchallenge.java
@@ -0,0 +1,13 @@
+package CharEng;
+
+public class Callchallenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] arr = {"m", "o"};
+		Challenge mLetter = new Challenge();
+		mLetter.missingLetter(arr);
+
+	}
+
+}
